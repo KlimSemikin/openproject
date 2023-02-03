b0f2cfdad54d9e0ccef7089d12be090a55888a96
klimsemikin <klimsemikin@gmail.com>
2023-02-03 10:07:21 +0300

    add method for get all custom_nested_option of tree with id

--- frontend/src/app/core/apiv3/endpoints/nested_options/api-v3-nested-options-paths.ts
+++ frontend/src/app/core/apiv3/endpoints/nested_options/api-v3-nested-options-paths.ts
@@ -4,6 +4,8 @@ import { NestedOptionResource } from 'core-app/features/hal/resources/nested-opt
 import { ApiV3Service } from 'core-app/core/apiv3/api-v3.service';
 import { ApiV3Collection } from 'core-app/core/apiv3/cache/cachable-apiv3-collection';
 import { StateCacheService } from 'core-app/core/apiv3/cache/state-cache.service';
+import { ApiV3ListParameters, listParamsString } from 'core-app/core/apiv3/paths/apiv3-list-resource.interface';
+import { CollectionResource } from 'core-app/features/hal/resources/collection-resource';
 
 export class ApiV3NestedOptionsPaths extends ApiV3Collection<NestedOptionResource, ApiV3NestedOptionPaths> {
   // Base path
@@ -15,9 +17,23 @@ export class ApiV3NestedOptionsPaths extends ApiV3Collection<NestedOptionResourc
   }
 
   /**
+   * Load a list of custom nested options with a given list parameter filter
+   *
+   * @param params
+   */
+  public list(params?:ApiV3ListParameters):Observable<CollectionResource<NestedOptionResource>> {
+    return this
+      .halResourceService
+      .get<CollectionResource<NestedOptionResource>>(this.path + listParamsString(params))
+      .pipe(
+        this.cacheResponse(),
+      );
+  }
+
+  /**
    * Create a custom nested option from a form payload          этот метод для создания нового элемента,
    *                                                            надо найти update для изменения родителя
-   *                                                            и get для списка всех опций
+   *
    * @param payload
    * @return {Promise<WorkPackageResource>}
    */
